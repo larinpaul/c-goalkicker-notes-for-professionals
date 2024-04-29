fc1bdf3954351909df7fa03f832ecd8eeea5f3b5
Pavel Larin <larinpavelfirst@mail.ru>
2024-04-29 06:15:31 -0300

    C Goalkicker Book for Professionals

---
+++ chapter1/main.c
@@ -0,0 +1,22 @@
+// Chapter 1: Getting started with C language
+
+
+// Section 1.1.: Hello World
+
+// To create a simple C program which prints "Hello World" on the screen,
+// use a text editor to create a new file (e.g. hello.c -
+// the file extension must be .c) containing the following source code:
+
+// hello.c
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello, World");
+    return 0;
+}
+
+
+
+
+
